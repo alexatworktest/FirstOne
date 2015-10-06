68724f7321feeef5cd4bfb960afdbdb2e99c320a
Alex <astreet@gdc-uk.org>
2015-10-06 13:17:07 +0100

    geget

---
+++ ClassLibraryGit1/IgnoreMe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryGit1
+{
+    public class IgnoreMe
+    {
+        // Not in the repo
+    }
+}
